2739e2897737be834ed973a91b5060c7c7ffb875
Joshua Martin <joshua.martin@noaa.gov>
2020-03-14 19:40:38 -0500

    fix file open

--- utils/dart_tools.py
+++ utils/dart_tools.py
@@ -702,7 +702,7 @@ def mrms_write_DART_ascii(obs, filename=None, obs_error=None, zero_dbz_obtype=_z
     # to read back in the entire contents of the obs-seq file, store it, rewrite the file
     # with header information first, and then dump the contents of obs-seq back inp.  Yuck.
 
-    with file(filename, 'r') as f: f_obs_seq = f.read()
+    with open(filename, 'r') as f: f_obs_seq = f.read()
 
     fi = open(filename, "w")
 
