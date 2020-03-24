470ac639c2b451385cd00756bed9fbd397e065ac
Joshua Martin <joshua.martin@noaa.gov>
2020-03-24 14:44:04 -0500

    added config for non-slurm use

--- slurm/jobs.py
+++ slurm/jobs.py
@@ -1,22 +1,33 @@
 import subprocess
 import datetime as DT
 from Config import settings
-
-# TODO: check config for "use_slurm", if false, spin off python processes
+from multiprocessing import Pool
+from utils.radar import getFromFile
+from pyOPAWS.slurm_run import main
 
 def runOPAWSForTime(run_time, totalRadars):
     date = run_time.strftime("%Y%m%d%H%M")
-    cmd = "JOBID=$(sbatch --job-name=opaws_%s --parsable --array=0-%i --export=CYCLETIME=%s jobs/opaws.job) " % (date, totalRadars-1, date)
-    cmd += "&& sbatch --job-name=opaws_combine_%s --export=COMBINETIME=%s --depend=afterany:$JOBID jobs/combine.job" % (date, run_time.strftime("%Y%m%d_%H%M"))
-    print(cmd)
-    OPAWSret = subprocess.Popen([cmd],shell=True)
-    OPAWSret.wait()        
-    print("\n Slurm_opaws job completed at %s" % DT.datetime.now().strftime("%H:%M:%S"))
+    if settings.default_slurm_enabled == True:
+        cmd = "JOBID=$(sbatch --job-name=opaws_%s --parsable --array=0-%i --export=CYCLETIME=%s jobs/opaws.job) " % (date, totalRadars-1, date)
+        cmd += "&& sbatch --job-name=opaws_combine_%s --export=COMBINETIME=%s --depend=afterany:$JOBID jobs/combine.job" % (date, run_time.strftime("%Y%m%d_%H%M"))
+        print(cmd)
+        OPAWSret = subprocess.Popen([cmd],shell=True)
+        OPAWSret.wait()
+    else:
+        pool = Pool(processes=totalRadars)
+        for i, _ in enumerate(getFromFile(run_time)):
+            pool.apply_async(main, (run_time, i))
+        pool.close()
+        pool.join()   
+    print("\n Slurm_opaws job submitted at %s" % DT.datetime.now().strftime("%H:%M:%S"))
 
 def runMRMSForTime(run_time):
     date = run_time.strftime("%Y%m%d%H%M")
-    cmd = "sbatch --job-name=mrms_%s --export=CYCLETIME=%s jobs/mrms.job" % (date, date)
+    if settings.default_slurm_enabled == True:
+        cmd = "sbatch --job-name=mrms_%s --export=CYCLETIME=%s jobs/mrms.job" % (date, date)
+    else:
+        cmd = "python -m pyMRMS.slurm_run --cycle %s" % date
     print(cmd)
     OPAWSret = subprocess.Popen([cmd],shell=True)
-    OPAWSret.wait()        
-    print("\n Slurm_mrms job completed at %s" % DT.datetime.now().strftime("%H:%M:%S"))
+    OPAWSret.wait()
+    print("\n Slurm_mrms job submitted at %s" % DT.datetime.now().strftime("%H:%M:%S"))
