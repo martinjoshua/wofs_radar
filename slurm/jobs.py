dbf4770a28ace154e943d47a402602bb43ef7c32
Joshua Martin <joshua.martin@noaa.gov>
2020-03-24 10:10:15 -0500

    update to make job name dynamic

--- slurm/jobs.py
+++ slurm/jobs.py
@@ -5,14 +5,17 @@ from Config import settings
 # TODO: check config for "use_slurm", if false, spin off python processes
 
 def runOPAWSForTime(run_time, totalRadars):
-    cmd = "JOBID=$(sbatch --parsable --array=0-%i --export=CYCLETIME=%s jobs/opaws.job) && sbatch --export=COMBINETIME=%s --depend=afterany:$JOBID jobs/combine.job" % (totalRadars-1, run_time.strftime("%Y%m%d%H%M"), run_time.strftime("%Y%m%d_%H%M"))
+    date = run_time.strftime("%Y%m%d%H%M")
+    cmd = "JOBID=$(sbatch --job-name=opaws_%s --parsable --array=0-%i --export=CYCLETIME=%s jobs/opaws.job) " % (date, totalRadars-1, date)
+    cmd += "&& sbatch --job-name=opaws_combine_%s --export=COMBINETIME=%s --depend=afterany:$JOBID jobs/combine.job" % (date, run_time.strftime("%Y%m%d_%H%M"))
     print(cmd)
     OPAWSret = subprocess.Popen([cmd],shell=True)
     OPAWSret.wait()        
     print("\n Slurm_opaws job completed at %s" % DT.datetime.now().strftime("%H:%M:%S"))
 
 def runMRMSForTime(run_time):
-    cmd = "sbatch --export=CYCLETIME=%s jobs/mrms.job" % run_time.strftime("%Y%m%d%H%M")
+    date = run_time.strftime("%Y%m%d%H%M")
+    cmd = "sbatch --job-name=mrms_%s --export=CYCLETIME=%s jobs/mrms.job" % (date, date)
     print(cmd)
     OPAWSret = subprocess.Popen([cmd],shell=True)
     OPAWSret.wait()        
